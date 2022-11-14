e12480df0f2869acda685212089d5b196c668e19
BetterCallAle <dap22091998@gmail.com>
2022-11-14 15:41:27 +0100

    added AppSection components

---
+++ src/components/AppSection.vue
@@ -0,0 +1,58 @@
+<script>
+    import {store} from "../store";
+    import AppCard from "./AppCard.vue";
+    export default{
+        name: "MoviesSection",
+        props:{
+            section: String
+        },
+        components:{
+            AppCard
+        },
+        data(){
+            return{
+                store
+            }
+        },
+        computed:{
+            // check if section is title or movies and change title name
+            sectionTitle(){
+                if(this.section === "movies"){
+                    return "title"
+                } else {
+                    return "name"
+                }
+            },
+
+             // check if section is title or movies and change original title name
+            sectionOriginalTitle(){
+                if(this.section === "movies"){
+                    return "original_title"
+                } else {
+                    return "original_name"
+                }
+            }
+        }
+    }
+</script>
+
+<template>
+    <section id="movies-section">
+        <div class="container">
+            <div class="section-heading">
+                <h2>{{ section }}</h2>
+            </div>
+
+            <div class="section-main">
+                <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+                    <div class="col" v-for="element in store[section]" :key="element.id">
+                        <AppCard :title="element[sectionTitle]" :originalTitle="element.original_name" :element="element"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+</style>
--- src/components/SeriesSection.vue
+++ src/components/SeriesSection.vue
@@ -1,35 +1,15 @@
 <script>
-    import {store} from "../store";
-    import AppCard from "./AppCard.vue";
+    import AppSection from "./AppSection.vue";
     export default{
-        name: "MoviesSection",
+        name: "SeriesSection",
         components:{
-            AppCard
-        },
-        data(){
-            return{
-                store
-            }
+            AppSection
         }
     }
 </script>
 
 <template>
-    <section id="movies-section">
-        <div class="container">
-            <div class="section-heading">
-                <h2>Series</h2>
-            </div>
-
-            <div class="section-main">
-                <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
-                    <div class="col" v-for="element in store.series" :key="element.id">
-                        <AppCard :title="element.name" :originalTitle="element.original_name" :element="element"/>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    <AppSection :section="'series'" />
 </template>
 
 <style lang="scss" scoped>
